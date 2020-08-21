3f26b8c076d220066e6ba6b5b8484496d32f828a
ivanfsilva <ivanfs27@gmail.com>
2020-08-20 22:41:29 -0300

    verificar se existe e verificaqr se não existe livro no banco

--- src/test/java/br/com/ivanfsilva/libraryapi/model/repository/BookRepositoryTest.java
+++ src/test/java/br/com/ivanfsilva/libraryapi/model/repository/BookRepositoryTest.java
@@ -1,5 +1,6 @@
 package br.com.ivanfsilva.libraryapi.model.repository;
 
+import br.com.ivanfsilva.libraryapi.model.entity.Book;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -27,11 +28,27 @@ public class BookRepositoryTest {
     public void returnTrueWhenIsbnExists() {
         // cenario
         String isbn = "123";
+        Book book = Book.builder().title("Aventuras").author("Fulano").isbn(isbn).build();
+
+        entityManager.persist(book);
 
         // execução
         boolean exists = repository.existsByIsbn(isbn);
 
         //verificacao
         assertThat(exists).isTrue();
+    }
+
+    @Test
+    @DisplayName("Deve retornar falso quando não existir um livro na base com o isbn informado")
+    public void returnFalseWhenIsbnDoesntExist() {
+        // cenario
+        String isbn = "123";
+
+        // execução
+        boolean exists = repository.existsByIsbn(isbn);
+
+        //verificacao
+        assertThat(exists).isFalse();
     }
 }
